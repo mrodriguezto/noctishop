977b2a4598297c11f871f41f62bb7a6a41a55410
mrodriguezto <mirdor.dev@gmail.com>
2022-09-06 18:18:20 -0500

    feat(middleware): validate session and roles for admin pages and api

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,76 @@ import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+  const session: any = await getToken({ req });
 
-  if (!token) {
-    const requestedPage = req.nextUrl.pathname;
-    const url = req.nextUrl.clone();
-    url.pathname = `/auth/login`;
-    url.search = `p=${requestedPage}`;
-    return NextResponse.redirect(url);
+  // API VALIDATIONS ------------------------------
+
+  const path = req.nextUrl.pathname;
+
+  if (path.startsWith('/api')) {
+    if (!session) {
+      return redirectToUnathorized(req);
+    }
+
+    const validApiRoles = ['admin'];
+
+    // Admin
+    if (path.startsWith('/api/admin')) {
+      if (!validApiRoles.includes(session.user.role)) {
+        return redirectToUnathorized(req);
+      }
+    }
+  }
+
+  // PAGES VALIDATIONS ---------------------------
+
+  if (!session) {
+    return redirectToLogin(req);
+  }
+
+  // Checkout
+  if (path.startsWith('/checkout')) {
+    return NextResponse.next();
+  }
+
+  // Admin
+  if (path.startsWith('/admin')) {
+    const validPagesRoles = ['admin'];
+
+    if (!validPagesRoles.includes(session.user.role)) {
+      return redirectToHome(req);
+    }
+
+    return NextResponse.next();
   }
 
   return NextResponse.next();
 }
 
+// API REDIRECTS
+
+function redirectToUnathorized(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  url.pathname = '/api/auth/unauthorized';
+  return NextResponse.redirect(url);
+}
+
+// PAGES REDIRECTS
+
+function redirectToLogin(req: NextRequest) {
+  const requestedPage = req.nextUrl.pathname;
+  const url = req.nextUrl.clone();
+  url.pathname = `/auth/login`;
+  url.search = `p=${requestedPage}`;
+  return NextResponse.redirect(url);
+}
+
+function redirectToHome(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  url.pathname = '/';
+  return NextResponse.redirect(url);
+}
+
 export const config = {
-  matcher: ['/checkout/:path*'],
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
 };
